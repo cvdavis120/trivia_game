02833ef1f790d4cb1619ed4d7eb5dffbcb0969a2
Colin D <colinv120@gmail.com>
2019-07-20 16:00:20 -0600

    created variables for js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,42 @@
-console.log("go");
+$("document").ready( function() {
+
+    const questionArray = [
+        [1, "This is questions number 1"],
+        [2, "This is question 2"],
+        [3, "this is question 3"],
+        [4, "this is question 4"],
+        [5, "this is qutions 5"]
+    ]
+    const answerArrayOne = ["a","b","c"]
+    const answerArrayTwo =["d","e","f"]
+    const answerArrayThree = ["g", "h", "i"]
+    const answerArrayFour = ["j", "k" ,"l"]
+    const answerArrayFive = ["m", "n", "o"]
+    
+    const correctAnswerOne = "b"
+    const correctAnswerTwo = "f"
+    const correctAnswerThree = "g"
+    const correctAnswerFour = "k"
+    const correctAnswerFive = "o"
+    
+    
+    let correctCount = 0
+    let wrongCount = 0
+    let userAnswer = ""
+    let userWins = 0
+    let questionCount = 0
+
+
+    
+
+function check 
+
+
+
+
+
+
+
+
+});
+
