7acb7d6ab992eb5982f8685c0b5ac725b49e81b7
Colin D <colinv120@gmail.com>
2019-07-20 16:14:13 -0600

    nonesense start button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,36 @@
-$("document").ready( function() {
+$("document").ready(function() {
+  // // Page stuff
+  $("#gameStart").click(function() {
+    $(".game").removeAttr("hidden");
+    $("#gameStart").hide();
+  });
 
-    const questionArray = [
-        [1, "This is questions number 1"],
-        [2, "This is question 2"],
-        [3, "this is question 3"],
-        [4, "this is question 4"],
-        [5, "this is qutions 5"]
-    ]
-    const answerArrayOne = ["a","b","c"]
-    const answerArrayTwo =["d","e","f"]
-    const answerArrayThree = ["g", "h", "i"]
-    const answerArrayFour = ["j", "k" ,"l"]
-    const answerArrayFive = ["m", "n", "o"]
-    
-    const correctAnswerOne = "b"
-    const correctAnswerTwo = "f"
-    const correctAnswerThree = "g"
-    const correctAnswerFour = "k"
-    const correctAnswerFive = "o"
-    
-    
-    let correctCount = 0
-    let wrongCount = 0
-    let userAnswer = ""
-    let userWins = 0
-    let questionCount = 0
+  // GAME
+  const questionArray = [
+    [1, "This is questions number 1"],
+    [2, "This is question 2"],
+    [3, "this is question 3"],
+    [4, "this is question 4"],
+    [5, "this is qutions 5"]
+  ];
+  const answerArrayOne = ["a", "b", "c"];
+  const answerArrayTwo = ["d", "e", "f"];
+  const answerArrayThree = ["g", "h", "i"];
+  const answerArrayFour = ["j", "k", "l"];
+  const answerArrayFive = ["m", "n", "o"];
 
+  const correctAnswerOne = "b";
+  const correctAnswerTwo = "f";
+  const correctAnswerThree = "g";
+  const correctAnswerFour = "k";
+  const correctAnswerFive = "o";
 
-    
+  let correctCount = 0;
+  let wrongCount = 0;
+  let userAnswer = "";
+  let userWins = 0;
+  let questionCount = 0;
 
-function check 
-
-
-
-
-
-
-
-
+  //moving the cards. one from another with the correct info displayed
+  //
 });
-
